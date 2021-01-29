1edce71b9542ab7fe9f40861bdf4bccf4a1be6ce
damianArgote <argotedamian@gmail.com>
2021-01-28 23:41:53 -0300

    Feature Sidebar and Navegation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavUserComponent } from './nav-user/nav-user.component';
+import { NavegationComponent } from './navegation/navegation.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    routingComponents
+    routingComponents,
+    NavUserComponent,
+    NavegationComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
